c1a6491aa3eb7800c5d18b54e25013303445ad1f
cleancoderocker <scalarocker@googlemail.com>
2016-11-30 16:22:30 +0100

    First version of dummy sensor example

--- test/DummySensorTest.js
+++ test/DummySensorTest.js
@@ -8,4 +8,14 @@ describe("imergo-sensor-dummy::DummySensor", () =>
         let sensor = new model.DummySensor();
         assert.equal(sensor instanceof model.DummySensor, true);
     });
+    it("should return random values", done =>
+    {
+        let sensor = new model.DummySensor();
+        sensor.onchange = event => {
+            assert(event.reading.dummyValue > 0 && event.reading.dummyValue < 1);
+            assert(event.reading.values.get("dummyValue") > 0 && event.reading.values.get("dummyValue") < 1);
+            done();
+        }
+        sensor.start();
+    });
 });
